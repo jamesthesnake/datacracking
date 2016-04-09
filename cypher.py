65b7696aee08b7a89c7809bb2d9cb82b0105f70d
jamesthesnake <james.ryan.hennessy@gmail.com>
2016-04-09 16:46:57 -0400

    Added files via upload

--- cypher.py
+++ cypher.py
@@ -1,17 +1,47 @@
 import base64
+import binascii
+#base 64 message
 dataText64="bHRzbmV0Lm5ldC9jaGFsbGVuZ2U"
+#secert key message in base 64
 keyData64 ="JwUTMy5NG3UfPnQrIQx3KyMIEicuAhY5DmEbeDZmHD82TRd+CE1jJT\
 cKbUo0FGYyNmcCPR8SAXwgBBdb"
+#theyellow string in base 64
 dataTextTwo64="RmluZExUUw=="
-dataTextBinaryTwo="01000110 01101001 01101110 01100100 01001100 01010100 01010011"
+ #the yellow stirng in binary
+dataTextBinaryTwo="01000110011010010110111001100100010011000101010001010011"
+#bsae 64 in binary
 dataTextBinary="01101100 01110100 01110011 01101110 01100101 01110100 00101110 01101110 01100101 01110100 00101111 01100011 01101000 01100001 01101100 01101100 01100101 01101110 01100111 01100101"
+#key in binary
 keyDataBinary= "00100111 00000101 00010011 00110011 00101110 01001101 00011011 01110101 00011111 00111110 01110100 00101011 00100001 00001100 01110111 00101011 00100011 00001000 00010010 00100111 00101110 00000010 00010110 00111001 00001110 01100001 00011011 01111000 00110110 01100110 00011100 00111111 00110110 01001101 00010111 01111110 00001000 01001101 01100011 00100101 00110111 00001010 01101101 01001010 00110100 00010100 01100110 00110010 00110110 01100111 00000010 00111101 00011111 00010010 00000001 01111100 00100000 00000100 00010111 01011011"
+#secert message
 keyDataHex="270513332e4d1b751f3e742b210c772b230812272e0216390e611b7836661c3f364d177e084d6325370a6d4a341466323667023d1f12017c2004175b"
 data = base64.b64decode(keyData64)
 key = dataText64
-for k in range( 1,12):
-    print(ord(keyData64[k]),ord(dataTextTwo64[k]))
-    print(keyDataBinary[k]^dataTextBinaryTwo[k])
+binaryString=""
+binaryStringTwo=""
+j=1
+m=1
+for k in range( 1,len(keyData64)):
+   # print(ord(keyData64[k]),ord(dataTextTwo64[k]))
+    #print(keyDataBinary[1:64],dataTextBinaryTwo)
+
+    sum=ord(dataTextBinaryTwo[m])^ord(keyDataBinary[k])
+    sumly= ord(dataTextBinary[j])^ord(keyDataBinary[k])
+    binaryString=binaryString+str(sumly)
+    binaryStringTwo=binaryStringTwo+str(sum)
+    if m==len(dataTextBinaryTwo):
+        m=1
+    else:
+        m=m+1
+    if j==len(dataTextBinary):
+        j=1
+    else:
+        j=j+1
+    print(sumly,chr(sumly))
+print(binaryString)
+print(binaryStringTwo)
+print(binascii.b2a_base64(sumly))
+
 S = range(256)
 j = 0
 out = []
