506e1dc49928dc479386d96b56878fbfdbdf10b4
jamesthesnake <james.ryan.hennessy@gmail.com>
2016-04-09 22:37:06 -0400

    Added files via upload

--- cypher.py
+++ cypher.py
@@ -1,47 +1,61 @@
 import base64
 import binascii
 #base 64 message
-dataText64="bHRzbmV0Lm5ldC9jaGFsbGVuZ2U"
+dataText64="bHRzbmV0Lm5ldC9jaGFsbGVuZ2U=bHRzbmV0Lm5ldC9jaGFsbGVuZ2U=bHRzbmV0Lm5ldC9jaGFsbGVuZ2U="
 #secert key message in base 64
-keyData64 ="JwUTMy5NG3UfPnQrIQx3KyMIEicuAhY5DmEbeDZmHD82TRd+CE1jJT\
+cypherData64 ="JwUTMy5NG3UfPnQrIQx3KyMIEicuAhY5DmEbeDZmHD82TRd+CE1jJT\
 cKbUo0FGYyNmcCPR8SAXwgBBdb"
 #theyellow string in base 64
 dataTextTwo64="RmluZExUUw=="
  #the yellow stirng in binary
 dataTextBinaryTwo="01000110011010010110111001100100010011000101010001010011"
 #bsae 64 in binary
-dataTextBinary="01101100 01110100 01110011 01101110 01100101 01110100 00101110 01101110 01100101 01110100 00101111 01100011 01101000 01100001 01101100 01101100 01100101 01101110 01100111 01100101"
+dataTextBinary="01101100011101000111001101101110011001010111010000101110 01101110011001010111010000101111011000110110100001100001011011000110110001100101011011100110011101100101"
 #key in binary
-keyDataBinary= "00100111 00000101 00010011 00110011 00101110 01001101 00011011 01110101 00011111 00111110 01110100 00101011 00100001 00001100 01110111 00101011 00100011 00001000 00010010 00100111 00101110 00000010 00010110 00111001 00001110 01100001 00011011 01111000 00110110 01100110 00011100 00111111 00110110 01001101 00010111 01111110 00001000 01001101 01100011 00100101 00110111 00001010 01101101 01001010 00110100 00010100 01100110 00110010 00110110 01100111 00000010 00111101 00011111 00010010 00000001 01111100 00100000 00000100 00010111 01011011"
+keyDataBinaryOne= 001001110000010100010011001100110010111001001101000110110111010100011111001111100111010000101011001000010000110001110111001010110010001100001000000100100010011100101110000000100001011000111001000011100110000100011011011110000011011001100110000111000011111100110110010011010001011101111110000010000100110101100011001001010011011100001010011011010100101000110100000101000110011000110010001101100110011100000010001111010001111100010010000000010111110000100000000001000001011101011011
+keyDataBinary=str(keyDataBinaryOne)
 #secert message
 keyDataHex="270513332e4d1b751f3e742b210c772b230812272e0216390e611b7836661c3f364d177e084d6325370a6d4a341466323667023d1f12017c2004175b"
-data = base64.b64decode(keyData64)
+#ascii  based on secert messge
+asCypher= binascii.unhexlify(keyDataHex)
+print(len(asCypher))
+print(len(dataText64))
+print(dataText64[26])
+data = base64.b64decode(cypherData64)
 key = dataText64
 binaryString=""
 binaryStringTwo=""
 j=1
 m=1
-for k in range( 1,len(keyData64)):
+for k in range( 1,len(asCypher)-1):
    # print(ord(keyData64[k]),ord(dataTextTwo64[k]))
     #print(keyDataBinary[1:64],dataTextBinaryTwo)
-
-    sum=ord(dataTextBinaryTwo[m])^ord(keyDataBinary[k])
-    sumly= ord(dataTextBinary[j])^ord(keyDataBinary[k])
-    binaryString=binaryString+str(sumly)
-    binaryStringTwo=binaryStringTwo+str(sum)
+ 
+#    sum=ord(dataTextBinaryTwo[m])^ord(keyDataBinary[k])
+#    sumly= ord(cypherData64[j])^ord(asCypher[k])
+     
+    sumly= ord(dataText64[j])^ord(asCypher[k])
+    print(chr(sumly))
+    
+    sumly= chr(sumly)
+    binaryString=binaryString+str(sumly)+" "
+ #   binaryStringTwo=binaryStringTwo+str(sum)
     if m==len(dataTextBinaryTwo):
         m=1
     else:
         m=m+1
-    if j==len(dataTextBinary):
+    if j==len(dataText64)-1:
         j=1
     else:
         j=j+1
-    print(sumly,chr(sumly))
+#    print(sumly,chr(sumly))
 print(binaryString)
-print(binaryStringTwo)
-print(binascii.b2a_base64(sumly))
-
+#print(binaryStringTwo)
+print(sumly)
+print(ord(cypherData64)^ord(asCypher))
+#print(binascii.b2a_base64(sumly))
+sumlyTwo= ord(dataTextBinary)^ord(keyDataBinary)
+print(char(sumlyTwo))
 S = range(256)
 j = 0
 out = []
